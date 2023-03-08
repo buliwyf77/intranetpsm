1ee67e1ab2d0fbed4683fc0b0cbdd76be96adb0e
Luis Torrealba <ltorrealba@psmservicios.cl>
2023-03-08 15:42:04 -0300

    api calendar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -163,7 +163,7 @@
   
         displayEventTime: false, // don't show the time column in list view
   
-        googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
+        googleCalendarApiKey: 'AIzaSyANHZczT4nHHhNOLiXSBvF0ByE0dWBaAfA',
   
         // US Holidays
         //events: 'en.usa#holiday@group.v.calendar.google.com',
