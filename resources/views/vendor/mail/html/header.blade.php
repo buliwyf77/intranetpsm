46276b5c3dfba94ed13ebd7deaeb5a262ad741b2
Luis Torrealba <ltorrealba@psmservicios.cl>
2023-04-25 09:06:11 -0400

    makrdown

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,9 @@
+<tr>
+<td class="header">
+    
+<a href="{{ $url }}">
+<!-- {{ $slot }} -->
+<img src="https://intranet.psmservicios.cl/template/dist/images/logo-psm.png" width="150">
+</a>
+</td>
+</tr>
